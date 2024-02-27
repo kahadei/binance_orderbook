2ce5eb3f9a1e1f8a63baa6a41de6868e609e5410
kahadei <kahadey2@gmail.com>
2024-02-27 20:50:35 +0200

    fixes

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://manhattan.foundation"
+    "https://manhattan.foundation"
 ]
 
 app.add_middleware(
